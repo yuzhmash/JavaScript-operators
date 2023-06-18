77fe65cf831c9c67964fb8a24cc825b8d3cfbe29
andrew <mymail@mail.me>
2023-06-18 20:55:56 +0200

    second commit

--- src/js/srcipt.js
+++ src/js/srcipt.js
@@ -9,4 +9,13 @@ console.log(4 + +' 3');
 let incr = 10,
     decr = 10;
 
-console.log(incr++);
+console.log(++incr); 
+
+console.log(5%2);
+
+const isCheked = true,
+      isClose = true;
+
+console.log(isCheked || isClose);
+
+console.log(45/9);
